a0fcd80c6875c11ccfe592192066302541f72454
BHT2001 <h52vnpj4kk@privaterelay.appleid.com>
2026-04-28 01:12:58 -0400

    Delete unit-5-final/final/script.js

--- unit-5-final/final/script.js
+++
@@ -1,8 +0,0 @@
-let graphbuttons = document.querySelectorAll(".graphbutton");
-let datatitle = document.getElementById("datatitle");
-let datadescription = document.getElementById("datadescription");
-
-graphbuttons.forEach(function(button) {button.addEventListener("click", function() {graphbuttons.forEach(function(otherButton) {otherButton.classList.remove("active");});
-
-button.classList.add("active");
-datatitle.textContent = button.dataset.title; datadescription.textContent = button.dataset.description;});});
